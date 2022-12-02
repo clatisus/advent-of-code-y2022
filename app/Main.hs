7223b1bbaa5112784a27948f0535aeecbf144b1b
clatisus <clatisus@gmail.com>
2022-12-02 14:44:20 +0800

    finish day 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,10 @@
 module Main where
 
 import Day1 (day1)
+import Day2 (day2)
 
 days :: [IO ()]
-days = [day1]
+days = [day1, day2]
 
 main :: IO ()
 main = do
---
+++ src/Day2.hs
@@ -0,0 +1,28 @@
+module Day2 (day2) where
+
+import Control.Arrow ((***))
+import Control.Monad (join)
+
+mapTuple :: (a -> b) -> (a, a) -> (b, b)
+mapTuple = join (***)
+
+encode :: Char -> Int
+encode x
+  | x == 'A' || x == 'X' = 0
+  | x == 'B' || x == 'Y' = 1
+  | x == 'C' || x == 'Z' = 2
+
+outcome :: Int -> Int -> Int
+outcome x y = ((y - x + 1) `mod` 3) * 3
+
+force :: Int -> Int -> Int
+force x y = (x + y - 1) `mod` 3
+
+score :: Int -> Int -> Int
+score x y = outcome x y + y + 1
+
+day2 :: IO ()
+day2 = do
+  input <- map (mapTuple encode . ((,) <$> head <*> last)) . lines <$> readFile "puzzle-input/day2"
+  print $ "part1: " <> show (sum . map (uncurry score) $ input)
+  print $ "part2: " <> show (sum . map (\(x, y) -> score x (force x y)) $ input)
